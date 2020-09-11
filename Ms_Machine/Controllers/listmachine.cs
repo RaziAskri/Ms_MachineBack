c0ff8850ac5312637ae82905cbaa1c3a5ec1f374
firaschaieb <fchaieb093@gmail.com>
2020-09-11 11:17:52 +0100

    hhh

---
+++ Ms_Machine/Controllers/listmachine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ms_Fournisseur.Controllers
+{
+    internal class listmachine
+    {
+        internal static int Count(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
